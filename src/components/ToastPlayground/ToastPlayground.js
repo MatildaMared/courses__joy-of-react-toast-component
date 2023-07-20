c64a6cccbc507643c10d7809f09f99e025d84720
Matilda Mared <matildamared@live.se>
2023-07-20 08:26:37 +0200

    solve exercise 02

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,12 +3,14 @@ import React from "react";
 import Button from "../Button";
 
 import styles from "./ToastPlayground.module.css";
+import Toast from "../Toast/Toast";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
 	const [message, setMessage] = React.useState("");
 	const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+	const [showToast, setShowToast] = React.useState(false);
 
 	const handleDisplayToast = () => {
 		console.log(
@@ -17,6 +19,7 @@ function ToastPlayground() {
 			"and variant:",
 			variant
 		);
+		setShowToast(true);
 	};
 
 	return (
@@ -25,6 +28,14 @@ function ToastPlayground() {
 				<img alt="Cute toast mascot" src="/toast.png" />
 				<h1>Toast Playground</h1>
 			</header>
+
+			{showToast && (
+				<Toast
+					message={message}
+					variant={variant}
+					onClose={() => setShowToast(false)}
+				/>
+			)}
 
 			<div className={styles.controlsWrapper}>
 				<div className={styles.row}>
